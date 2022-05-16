ab6852000a1683715ebf766c7297bc8044d9e971
Andi Fifian Ibnu Romdhon <70189494+andififian@users.noreply.github.com>
2022-05-16 19:43:08 +0700

    updating intent in every button

--- app/src/main/java/com/example/finalproject2/itemform.java
+++ app/src/main/java/com/example/finalproject2/itemform.java
@@ -6,19 +6,47 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class itemform extends AppCompatActivity {
     Spinner combo;
-
+    Button btnsubmitbrg;
+    TextInputEditText nama_brg, quantity;
+    private String namabar, kuantitas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_itemform);
 
+        nama_brg = findViewById(R.id.nama_bar);
+        quantity = findViewById(R.id.kuantitas_brg);
         combo = findViewById(R.id.spinnerbarang);
+        btnsubmitbrg = findViewById(R.id.btnsubmitbrg);
+        btnsubmitbrg.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View notif) {
+                namabar = nama_brg.getText().toString();
+                kuantitas = quantity.getText().toString();
 
+                if (notif.getId() == R.id.btnsubmitbrg) {
+                    if (namabar.equals("")) {
+                        Toast.makeText(getApplicationContext(), "nama barang tidak boleh kosong!", Toast.LENGTH_SHORT).show();
+                    }else if (kuantitas.equals("")) {
+                        Toast.makeText(getApplicationContext(), "Kuantitas barang tidak boleh kosong!", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "berhasil memasukkan Barang baru!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+
+        });
     }
 
     public void main(View view) { startActivity(new Intent(itemform.this, MainActivity.class));
--- app/src/main/java/com/example/finalproject2/newstaffform.java
+++ app/src/main/java/com/example/finalproject2/newstaffform.java
@@ -1,18 +1,64 @@
 package com.example.finalproject2;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class newstaffform extends AppCompatActivity {
+
+    private TextInputEditText namaleng, newstaffuser, passstaff, confpassstaff;
+    private Button btnsubmit;
+
+    private String namauser, usernewstaff,password, confpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newstaffform);
+
+
+        namaleng = findViewById(R.id.namaleng);
+        newstaffuser = findViewById(R.id.newstaffuser);
+        passstaff = findViewById(R.id.passstaff);
+        confpassstaff = findViewById(R.id.conpassstaff);
+        btnsubmit = findViewById(R.id.btnsubmit);
+        btnsubmit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View notif) {
+                namauser = namaleng.getText().toString();
+                usernewstaff = newstaffuser.getText().toString();
+                password = passstaff.getText().toString();
+                confpassword = confpassstaff.getText().toString();
+
+                if (notif.getId() == R.id.btnsubmit) {
+                    if (namaleng.equals("")) {
+                        Toast.makeText(getApplicationContext(), "nama tidak boleh kosong!", Toast.LENGTH_SHORT).show();
+
+                    }else if (usernewstaff.equals("")) {
+                        Toast.makeText(getApplicationContext(), "User tidak boleh kosong!", Toast.LENGTH_SHORT).show();
+
+                    }else if (password.equals("")) {
+                        Toast.makeText(getApplicationContext(), "Password tidak boleh kosong", Toast.LENGTH_SHORT).show();
+                    }else {
+                        Toast.makeText(getApplicationContext(), "berhasil mendaftarkan Staff baru!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+
+        });
     }
     public void main(View view) { startActivity(new Intent(newstaffform.this, MainActivity.class));
     }
+
+
+
 }
